e4a82111d0029c7f9163282c7f7b76948a6768f6
Khrystyna <khrystynkakitty@gmail.com>
2016-11-15 12:01:02 +0000

    text

---
+++ js/index.js
@@ -0,0 +1,15 @@
+
+btn.addEventListener('click', makeAjax);
+
+function makeAjax() {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var clientData = JSON.parse(xhr.responseText);
+            name.innerHTML = clientData.name;
+            address.innerHTML = clientData.address;
+        }
+    }
+    xhr.open('GET', 'data.json', true);
+    xhr.send();
+}
